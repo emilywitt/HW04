729597e43e833a94c073f28729d16f13989b7d7d
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-09 16:23:58 -0700

    EW 04_08_12

--- HW04_ex08_12.py
+++ HW04_ex08_12.py
@@ -1,3 +1,48 @@
 # Structure this script entirely on your own.
 # See Chapter 8: Strings Exercise 12 for guidance
 # Please do provide function calls that test/demonstrate your function
+# You might want to use the built-in functions ord, which converts a character 
+# to a numeric code, and chr, which converts numeric codes to characters.
+
+# how do you cycle back through the beginning
+
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# Imports
+
+# Body
+def rotate_letter(letter, n):
+    if letter.isupper():
+        start = ord('A')
+    elif letter.islower():
+        start = ord('a') # I get why we have to do this. but why is the return value 97?
+    else:
+        return letter
+    #print start
+    #print ord(letter)
+    c = ord(letter) - start
+    #print c
+    i = (c+n)%26 + start
+    #print i
+    return chr(i)
+
+def rotate_word(word, n):
+    rotated_word = ''
+    for letter in word:
+        rotated_word = rotated_word + rotate_letter(letter,n) 
+    return rotated_word
+    
+
+
+########
+def main():
+
+    #print rotate_letter("b", 13)
+    print rotate_word("balk",13)
+    print rotate_word('cheer', 7)
+    print rotate_word('melon', -10)
+    print rotate_word('melon', -26)
+
+
+if __name__ == '__main__':
+    main()  
