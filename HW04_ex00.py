e8232591cfa86fbb2e745692a8e3e0dbc9db8178
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-09 18:09:33 -0700

    random number game

--- HW04_ex00.py
+++ HW04_ex00.py
@@ -9,12 +9,42 @@
 #         - if not: tells them too high/low
 #     - only lets the user guess five times
 #         - then ends the program
+
+
+# after user enters wrong guess, have to send back into asking for another input
+# need to count the attempts (maybe use a while loop for this?)
+# what if I make n the number of tries, and use while n <= 5?
 ################################################################################
 # Imports
-
+import random
 
 # Body
 
+def number_game():
+	n = 0
+	r = random.randint(1,25)
+	#r = 15
+	while n <= 4:
+		g = raw_input ("You have " + str(5-n) +" chances to guess what number between 1 and 25 I'm holding behind my back.>")
+		try:
+			g == int(g)
+			if int(g) == r:
+				print "Great job!"
+				return
+			if int(g) < r:
+				print ("Too Low")
+			if int(g) > r:
+				print ("Too High")
+		except:
+			print ("You just wasted one of your 5 chances. That's not even a number!")
+		n = n + 1
+	print "Sorry! You have run out of chances."
+		
+
+# after user enters wrong guess, have to send back into asking for another input
+# need to count the attempts (maybe use a for loop for this, since number of attempts is a known quatity (would use while for unknown quantity?)?)
+# what if I make n the number of tries, and use while n <= 5? and then employ a counter count = 0, each time through it becomes count = count + 1
+# use a search for exiting when we find what we're looking for
 
 
 
@@ -22,8 +52,8 @@
 def main():
 
 
-    print("Hello World!") # Remove this and replace with your function calls
-    
+    #print("Hello World!") # Remove this and replace with your function calls
+    number_game()
 
 if __name__ == '__main__':
     main()
